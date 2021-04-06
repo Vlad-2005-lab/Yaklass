128b71a8722e106100a8d945af242ee717c5df84
Vlad <vladanikanov19@gmail.com>
2021-04-07 00:06:53 +0500

    update v0.3.2 heroku update

---
+++ push.py
@@ -0,0 +1,5 @@
+# text = open("requirements.txt", mode="r", encoding="utf-8").read().replace("\t", " ").split("\n")
+# file = open("requirements.txt", mode="w", encoding="utf-8")
+# for i in text:
+#     file.write("==".join(i.split()[: 2]) + "\n")
+# file.close()
